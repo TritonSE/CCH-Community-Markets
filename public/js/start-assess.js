7a6953ab5858ee5e4c8ea6013e98ff890fbd92c6
Jason Cabrera <cabrerajason7@gmail.com>
2019-01-02 11:58:57 -0800

    prevent button default behavior

--- public/js/start-assess.js
+++ public/js/start-assess.js
@@ -1,4 +1,7 @@
 //once pre-assess is over, displays assessment questions
+$('#pre-assess-button').click(function(event){
+    event.preventDefault();
+});
 $('#pre-assess-button').click(function(){
     $('h2.title').hide();
     $('#pre-assess-container').hide();
