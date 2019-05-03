2469bef64c6cce50350d6119c70642327bc36d66
Declan Sullivan <declan@declansullivan.com>
2019-05-03 00:18:56 -0700

    Marketdata page implemented

--- public/js/marketdata.js
+++ public/js/marketdata.js
@@ -6,4 +6,59 @@ $(document).ready(function() {
     console.log(sessionStorage);
     
     $('.text-dark').append(marketName);
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+    
+    // Creates connection to database.
+    var db = firebase.database();
+    // Links to head of database.
+    var ref = db.ref("live_weller");
+    // Links to markets list.
+    var marketsRef = ref.child("markets");
+
+    marketsRef.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+
+            var name = childData.marketInfo.marketName;
+            var address = childData.marketInfo.address;
+            var size = childData.marketInfo.storeType;
+            var zip = childData.marketInfo.zip;
+            var level = childData.marketInfo.marketLevel;
+
+            var firstName = childData.personalInfo.firstName;
+            var lastName = childData.personalInfo.lastName;
+            var email = childData.personalInfo.email;
+
+            var questions = childData.questions;
+
+            if (name === marketName) {
+
+                $('.text-dark').append("<br>");
+                $('.text-dark').append("Market Level: " + level);
+                $('.text-dark').append("<br>");
+                $('.text-dark').append("Address: " + address);
+                $('.text-dark').append("<br>");
+                $('.text-dark').append("Size: " + size);
+                $('.text-dark').append("<br>");
+
+                $('.text-subdark').append("Reviewer: " + firstName + " " + lastName);
+                $('.text-subdark').append("<br>");
+                $('.text-subdark').append("Email: " + email);
+                $('.text-subdark').append("<br>");
+
+                for (var key in questions) {
+                    if (key !== "undefined") {
+                        $('.text-muted').append(key + ": " + questions[key]);
+                        $('.text-muted').append("<br>");
+                    }
+                }
+
+                
+            }
+        });
+    });
 });
