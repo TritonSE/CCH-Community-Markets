18e49d99d1444807e36ae96d844f8c80c37de99e
Declan Sullivan <declan@declansullivan.com>
2019-05-03 13:26:01 -0700

    Added address

--- public/js/markets.js
+++ public/js/markets.js
@@ -33,7 +33,7 @@ $(document).ready( function () {
           var markup = "<tr><td>" + "<a id='turnblue' onclick='redirectFunction(this)'>"+name+"</a>" + "</td><td>" + address + 
                       "</td><td>" + size + "</td><td>" + zip + 
                       "</td><td>" + level + "</td><td><button class=\"mapButton\"" +
-                      "onclick=\"window.open('https://www.google.com/maps/dir/?api=1&destination=University of California, San Diego')\">" + 
+                      "onclick=\"window.open('https://www.google.com/maps/dir/?api=1&destination=" + address + ")\">" + 
                       "<p>Go!</p></button></td></tr>"
           $("#table_id").append(markup);
     });
