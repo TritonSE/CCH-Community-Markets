e28e714be93e8c2913536923f9629de6045b140c
ksoloway <ksoloway@ucsd.edu>
2019-05-31 14:43:46 -0700

    removed env from files

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
-// var config = {
-//     apiKey: process.env.apiKey,
-//     authDomain: process.env.authDomain,
-//     databaseURL: process.env.databaseURL,
-//     projectId: process.env.projectId,
-//     storageBucket: process.env.storageBucket,
-//     messagingSenderId: process.env.messagingSenderId
-// };
+ var config = {
+     apiKey: process.env.apiKey,
+     authDomain: process.env.authDomain,
+     databaseURL: process.env.databaseURL,
+     projectId: process.env.projectId,
+     storageBucket: process.env.storageBucket,
+     messagingSenderId: process.env.messagingSenderId
+ };
