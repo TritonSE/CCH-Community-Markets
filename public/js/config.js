cbaad0455f0e572f50f64546337cfb6d838a191c
Hannah Hsu <hannahhsu977@gmail.com>
2019-05-31 15:41:39 -0700

    Config vars

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,9 @@
- //var config = {
-  //   apiKey: app.get('apiKey'),
-    // authDomain: app.get('authDomain'),
-  //   databaseURL: app.get('databaseURL'),
-  //   projectId: app.get('projectId'),
-  //   storageBucket: app.get('storageBucket'),
-  //   messagingSenderId: app.get('messagingSenderId')
- //};
+var app = express()
+var config = {
+    apiKey: app.get('apiKey'),
+    authDomain: app.get('authDomain'),
+    databaseURL: app.get('databaseURL'),
+    projectId: app.get('projectId'),
+    storageBucket: app.get('storageBucket'),
+    messagingSenderId: app.get('messagingSenderId')
+};
