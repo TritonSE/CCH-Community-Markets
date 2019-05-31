dbbf7943ed395da43744fe30f680fc5cfc4df54c
ksoloway <ksoloway@ucsd.edu>
2019-05-31 14:48:36 -0700

    removed env from files

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
  var config = {
-     apiKey: apiKey,
-     authDomain: authDomain,
-     databaseURL: databaseURL,
-     projectId: projectId,
-     storageBucket: storageBucket,
-     messagingSenderId: messagingSenderId
+     apiKey: app.get('apiKey'),
+     authDomain: app.get('authDomain'),
+     databaseURL: app.get('databaseURL'),
+     projectId: app.get('projectId'),
+     storageBucket: app.get('storageBucket'),
+     messagingSenderId: app.get('messagingSenderId')
  };
