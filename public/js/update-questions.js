985dc214a16288ddaed4e12997cd8e845d02be56
Jason Cabrera <cabrerajason7@gmail.com>
2019-07-17 15:58:19 -0700

    cleaner implementation for update-questions.js

--- public/js/update-questions.js
+++ public/js/update-questions.js
@@ -3,8 +3,8 @@
 //questions are 1, 8, 11, 25, 28, 36
 $('input').change(function(){
     var id = $(this).attr("id")
-    var currentNumber = id.match(/\d+/g)
-    var currentNumber = parseInt(currentNumber)
+    var currentNumber = id.match(/\d+/g);
+    var currentNumber = parseInt(currentNumber);
     
     //array of questions that require greying out 
     var currentOptions = [1, 8, 11, 25, 28, 36]
@@ -30,82 +30,91 @@ $('input').change(function(){
     //if the current number requires greying out
     if( end != -1 ){
       
-      //handles questions with yes/no options
+      //booleans for reversing numOptions (grey/ungrey) and "Skip to ..." label (show/hide)
+      var reverseLabel = false
+      var reverseNumOptions = false
+
+      //loops through affected questions 
       for(let i = currentNumber + 1; i < end; i++){
 
-          //disable buttons if no was selected for current number
-          if($('#No-'+ currentNumber).is(":checked")){
-              $('#Yes-'+i).attr('disabled',true);
-              $('#No-'+i).attr('disabled',true);
-          } 
+        //disables questions if no is selected for current question
+        if($('#No-'+ currentNumber).is(":checked")){
           
-          //enable buttons again if yes selected for current number
-          else{
-            $('#Yes-'+i).attr('disabled',null);
-            $('#No-'+i).attr('disabled', null);
+          //shows "Skip to ..." label 
+          if(!reverseLabel){
+            reverseLabel = true
+            $('#'+currentNumber+'.if-no').show()
           }
-
-      }
-      
-      //disable number options if necessary 
-      if($('#No-' + currentNumber).is(":checked")){
-        
-        //show the label that says to skip to end
-        $('#'+currentNumber+'.if-no').show();
-
-        //disable options for numbers 4 and 7 if current number is 1
-        if(currentNumber == 1){
-          $('#0-4').attr("disabled", true);
-          $('#1-2-4').attr("disabled", true);
-          $('#3-5-4').attr("disabled", true);
-          $('#10-4').attr("disabled", true);
-          $('#6-9-4').attr("disabled", true);
-          $('#0-7').attr("disabled", true);
-          $('#1-2-7').attr("disabled", true);
-          $('#3-5-7').attr("disabled", true);
-          $('#6-9-7').attr("disabled", true);
-          $('#10-7').attr("disabled", true);
-        }
-        
-        //disable options for number 14 if current number is 11
-        if(currentNumber == 11){
-          $('#0-14').attr("disabled", true);
-          $('#1-2-14').attr("disabled", true);
-          $('#3-5-14').attr("disabled", true);
-          $('#6-9-14').attr("disabled", true);
-          $('#10-14').attr("disabled", true);
-        }
-      }
-      
-      //enable number options instead
-      else{
-        
-        //hide label that says skip to end
-        $('#'+currentNumber+'.if-no').hide();
-
-        //enable options for numbers 4 and 7 if current number is 1
-        if(currentNumber == 1){
-          $('#0-4').attr("disabled", null);
-          $('#1-2-4').attr("disabled", null);
-          $('#3-5-4').attr("disabled", null);
-          $('#10-4').attr("disabled", null);
-          $('#6-9-4').attr("disabled", null);
-          $('#0-7').attr("disabled", null);
-          $('#1-2-7').attr("disabled", null);
-          $('#3-5-7').attr("disabled", null);
-          $('#6-9-7').attr("disabled", null);
-          $('#10-7').attr("disabled", null);
-       }
-        
-        //enable options for number 14 if current number is 11
-        if(currentNumber == 11){
+              
+          //disables number options affected by current number (1) only once
+          if( !reverseNumOptions && currentNumber == 1 ){
+            reverseNumOptions = true;
+            $('#0-4').attr("disabled", true);
+            $('#1-2-4').attr("disabled", true);
+            $('#3-5-4').attr("disabled", true);
+            $('#10-4').attr("disabled", true);
+            $('#6-9-4').attr("disabled", true);
+            $('#0-7').attr("disabled", true);
+            $('#1-2-7').attr("disabled", true);
+            $('#3-5-7').attr("disabled", true);
+            $('#6-9-7').attr("disabled", true);
+            $('#10-7').attr("disabled", true);
+          }
+          
+          //disables number options affected by current number (11) only once 
+          if( !reverseNumOptions && currentNumber == 11 ){
+            reverseNumOptions = true;
+            $('#0-14').attr("disabled", true);
+            $('#1-2-14').attr("disabled", true);
+            $('#3-5-14').attr("disabled", true);
+            $('#6-9-14').attr("disabled", true);
+            $('#10-14').attr("disabled", true);
+          }
+          
+          //disables yes/no questions up till end
+          $('#Yes-'+i).attr('disabled',true);
+          $('#No-'+i).attr('disabled',true);
+        } 
+          
+        //enables questions otherwise
+        else{
+          
+          //hides "Skip to ..." label
+          if( !reverseLabel ) {
+            reverseLabel = true;
+            $('#'+currentNumber+'.if-no').hide();
+          }
+          
+          //enables number options affected by current number (1) only once
+          if( !reverseNumOptions && currentNumber == 1 ){
+            reverseNumOptions = true;
+            $('#0-4').attr("disabled", null);
+            $('#1-2-4').attr("disabled", null);
+            $('#3-5-4').attr("disabled", null);
+            $('#10-4').attr("disabled", null);
+            $('#6-9-4').attr("disabled", null);
+            $('#0-7').attr("disabled", null);
+            $('#1-2-7').attr("disabled", null);
+            $('#3-5-7').attr("disabled", null);
+            $('#6-9-7').attr("disabled", null);
+            $('#10-7').attr("disabled", null);
+          }      
+          
+          //enables number options affected by current number (11) only once
+          if( !reverseNumOptions && currentNumber == 11){
+            reverseNumOptions = true;
             $('#0-14').attr("disabled", null);
             $('#1-2-14').attr("disabled", null);
             $('#3-5-14').attr("disabled", null);
             $('#6-9-14').attr("disabled", null);
             $('#10-14').attr("disabled", null);
+          }
+          
+          //enables yes/no questions up till end
+          $('#Yes-'+i).attr('disabled', null);
+          $('#No-'+i).attr('disabled', null);
         }
 
-    }
+      }
   }
 });
