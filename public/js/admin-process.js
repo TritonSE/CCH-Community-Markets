0633c71ef86c41b7bbe92cfa09acfc76d79062fc
Declan Sullivan <declan@declansullivan.com>
2019-09-11 15:12:57 -0700

    Removed duplicated code in admin-process.js

--- public/js/admin-process.js
+++ public/js/admin-process.js
@@ -1,19 +1,5 @@
 
 $(document).ready( function () {
-    console.log("here");
-    var loggedIn = sessionStorage.getItem('loggedIn');
-    console.log(loggedIn);
-    if (loggedIn == "true") {
-        console.log("enable");
-        document.getElementById("closemarket").href = "/markets";
-        document.getElementById("closedata").href = "/data";
-    }
-    else {
-        console.log("disable");
-        document.getElementById("closemarket").href = "/admin-login";
-        document.getElementById("closedata").href = "/admin-login";
-    }
-
     if (!firebase.apps.length) {
         firebase.initializeApp(config);
     }
