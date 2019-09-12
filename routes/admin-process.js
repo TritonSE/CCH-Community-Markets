46477d31f459d328d8dff61920c95c85669612ed
Declan Sullivan <declan@declansullivan.com>
2019-09-12 15:45:14 -0700

    Removed logging

--- routes/admin-process.js
+++ routes/admin-process.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-
-console.log("hello");
 
 router.get('/', function(req, res, next) {
-	console.log(req.app.get('test'));
-	console.log("yo");
 	res.render('admin-process');
 });
 
