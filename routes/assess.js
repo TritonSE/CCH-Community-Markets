9526d7d8cb56157a0a6aeb3de6c85fc28ff2eae5
Jason Cabrera <cabrerajason7@gmail.com>
2019-09-16 16:39:48 -0700

    moved firebase code to assess route

--- routes/assess.js
+++ routes/assess.js
@@ -1,8 +1,37 @@
 var express = require('express');
+var firebase = require('firebase');
+var config = require('./config.js');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('assess');
+	
+	if(!firebase.apps.length) {
+     firebase.initializeApp(config);
+	}
+   console.log("sadfasd");
+	// Setup database communication.
+	var db = firebase.database();
+	var ref = db.ref("live_weller");
+
+	// Move to sub-directory.
+	var marketsRef = ref.child("markets");
+	var data = "";
+
+	console.log("here");
+	marketsRef.once('value', function(snapshot) {
+   	snapshot.forEach(function(childSnapshot) {
+   		var childKey = childSnapshot.key;
+      	var childData = childSnapshot.val();
+			
+			var markup = "<option>" + childKey + "</option>";
+			data += markup;
+   	});
+
+		data += "<option>" + "NEW MARKET" + "</option>";		 	
+	});
+
+
+	res.render('assess', {data: data});
 });
 
 module.exports = router;
