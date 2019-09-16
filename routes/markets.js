5d67427fe4b4165661238a3eec10c110165be61d
Declan Sullivan <declan@declansullivan.com>
2019-09-16 15:34:55 -0700

    Displays markets in ejs from routes

--- routes/markets.js
+++ routes/markets.js
@@ -1,8 +1,44 @@
 var express = require('express');
+var firebase = require('firebase');
+var config = require('./config.js');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('markets');
+	if (!firebase.apps.length) {
+		firebase.initializeApp(config.info());
+	  }
+	
+	// Creates connection to database.
+	var db = firebase.database();
+	// Links to head of database.
+	var ref = db.ref("live_weller");
+	// Links to markets list.
+	var marketsRef = ref.child("markets");
+
+	var dbinfo = "";
+
+	marketsRef.once('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			var childKey = childSnapshot.key;
+			var childData = childSnapshot.val();
+
+			var name = childData.marketInfo.marketName;
+			var address = childData.marketInfo.address;
+			var size = childData.marketInfo.storeType;
+			var zip = childData.marketInfo.zip;
+			var level = childData.marketInfo.marketLevel;
+			
+			var markup = "<tr><td>" + "<a id='turnblue' onclick='redirectFunction(this)'>"+name+"</a>" + "</td><td>" + address + 
+						"</td><td>" + size + "</td><td>" + zip + 
+						"</td><td>" + level + "</td><td><button class=\"mapButton\"" +
+						"onclick=\"window.open('https://www.google.com/maps/dir/?api=1&destination=" + address + "')\">" + 
+						"<p>Go!</p></button></td></tr>"
+			dbinfo += markup;
+		});
+
+		res.render('markets', {marketList: dbinfo});
+		// $('#table_id').DataTable();
+	});
 });
 
 module.exports = router;
