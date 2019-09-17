0ae047301b8dcfa351cec228816c952e2996e262
Declan Sullivan <declan@declansullivan.com>
2019-09-17 16:52:18 -0700

    Removed redirectFunction

--- public/js/markets.js
+++ public/js/markets.js
@@ -1,9 +1,5 @@
 if (sessionStorage.getItem('loggedIn') != "true") {location.href = "/admin-login";}
 
-function redirectFunction(choice){
-  location.href='marketdata/' + choice.text;
-}
-
 $(document).ready( function () {
   $('#table_id').DataTable();
 });
