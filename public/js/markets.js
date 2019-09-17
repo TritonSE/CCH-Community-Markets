060542bc9d9b3bbbbf504a2911132ee751b48e02
Declan Sullivan <declan@declansullivan.com>
2019-09-17 12:43:33 -0700

    Removed clientside code for redirection

--- public/js/markets.js
+++ public/js/markets.js
@@ -1,12 +1,7 @@
 if (sessionStorage.getItem('loggedIn') != "true") {location.href = "/admin-login";}
 
-// let markets = $('#table_id').DataTable();
-let assessmentArray = new Array();
 function redirectFunction(choice){
-  console.log(choice.text);
-  console.log("Function Entered");
-  sessionStorage.setItem("marketName",choice.text);
-  location.href='marketdata';
+  location.href='marketdata/' + choice.text;
 }
 
 $(document).ready( function () {
