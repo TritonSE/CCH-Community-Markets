563b51719dc2c3980b5e9c5be87058c02f578993
Declan Sullivan <declan@declansullivan.com>
2019-09-17 16:52:45 -0700

    var -> const and let, moved firebase info

--- routes/marketdata.js
+++ routes/marketdata.js
@@ -1,42 +1,41 @@
-var express = require('express');
-var firebase = require('firebase');
-var config = require('./config.js');
-var router = express.Router();
+const express = require('express');
+const firebase = require('firebase');
+const config = require('./config.js');
+const router = express.Router();
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config.config);
+}
+// Creates connection to database.
+const db = firebase.database();
+// Links to head of database.
+const ref = db.ref("live_weller");
+// Links to markets list.
+const marketsRef = ref.child("markets");
 
 router.get('/:marketName', function(req, res, next) {
-    var marketName = req.params.marketName;
-    
-    if (!firebase.apps.length) {
-        firebase.initializeApp(config.info());
-    }
-    
-    // Creates connection to database.
-    var db = firebase.database();
-    // Links to head of database.
-    var ref = db.ref("live_weller");
-    // Links to markets list.
-    var marketsRef = ref.child("markets");
+    const marketName = req.params.marketName;
 
     marketsRef.once('value', function(snapshot) {
-		var text_dark = marketName;
-		var text_subdark = "";
-		var text_muted_2 = "";
-		var text_subdark_3 = "";
-		var text_muted_3 = "";
+		let text_dark = marketName;
+		let text_subdark = "";
+		let text_muted_2 = "";
+		let text_subdark_3 = "";
+		let text_muted_3 = "";
         snapshot.forEach(function(childSnapshot) {
-            var childData = childSnapshot.val();
+            let childData = childSnapshot.val();
 
-            var name = childData.marketInfo.marketName;
-            var address = childData.marketInfo.address;
-            var size = childData.marketInfo.storeType;
-            var level = childData.marketInfo.marketLevel;
+            let name = childData.marketInfo.marketName;
+            let address = childData.marketInfo.address;
+            let size = childData.marketInfo.storeType;
+            let level = childData.marketInfo.marketLevel;
 
-            var firstName = childData.personalInfo.firstName;
-            var lastName = childData.personalInfo.lastName;
-            var email = childData.personalInfo.email;
+            let firstName = childData.personalInfo.firstName;
+            let lastName = childData.personalInfo.lastName;
+            let email = childData.personalInfo.email;
 
-            var questions = childData.questions;
-            var missed = childData.missedQuestions;
+            let questions = childData.questions;
+            let missed = childData.missedQuestions;
 
             if (name === marketName) {
 				text_dark += "<br>";
@@ -52,7 +51,7 @@ router.get('/:marketName', function(req, res, next) {
                 text_subdark += "Email: " + email;
                 text_subdark += "<br>";
 
-                for (var key in questions) {
+                for (let key in questions) {
                     if (key !== "undefined") {
                         text_muted_2 += key + ": " + "<span class=\"boldanswer\">" + questions[key] + "</span>";
                         text_muted_2 += "<br>";
@@ -65,12 +64,12 @@ router.get('/:marketName', function(req, res, next) {
                     text_subdark_3 += "Questions to fix to get to level " + (parseInt(level) + 1);
                 }
 
-                for (var key in missed) {
+                for (let key in missed) {
                     text_muted_3 += missed[key];
                     text_muted_3 += "<br>";
                 }
             }
-		});
+        });
 		
 		res.render('marketdata', {td: text_dark, tsd: text_subdark, tm2: text_muted_2, tsd3: text_subdark_3, tm3: text_muted_3});
     });
--- routes/markets.js
+++ routes/markets.js
@@ -1,41 +1,35 @@
-var express = require('express');
-var firebase = require('firebase');
-var config = require('./config.js');
-var router = express.Router();
+const express = require('express');
+const firebase = require('firebase');
+const config = require('./config.js');
+const router = express.Router();
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(config.config);
+}
+// Creates connection to database.
+const db = firebase.database();
+// Links to head of database.
+const ref = db.ref("live_weller");
+// Links to markets list.
+const marketsRef = ref.child("markets");
 
 router.get('/', function(req, res, next) {
-	if (!firebase.apps.length) {
-		firebase.initializeApp(config.info());
-	  }
-	
-	// Creates connection to database.
-	var db = firebase.database();
-	// Links to head of database.
-	var ref = db.ref("live_weller");
-	// Links to markets list.
-	var marketsRef = ref.child("markets");
-
-	var dbinfo = "";
+	let markets = [];
 
 	marketsRef.once('value', function(snapshot) {
 		snapshot.forEach(function(childSnapshot) {
-			var childData = childSnapshot.val();
+			let childData = childSnapshot.val();
 
-			var name = childData.marketInfo.marketName;
-			var address = childData.marketInfo.address;
-			var size = childData.marketInfo.storeType;
-			var zip = childData.marketInfo.zip;
-			var level = childData.marketInfo.marketLevel;
-			
-			var markup = "<tr><td>" + "<a id='turnblue' onclick='redirectFunction(this)'>"+name+"</a>" + "</td><td>" + address + 
-						"</td><td>" + size + "</td><td>" + zip + 
-						"</td><td>" + level + "</td><td><button class=\"mapButton\"" +
-						"onclick=\"window.open('https://www.google.com/maps/dir/?api=1&destination=" + address + "')\">" + 
-						"<p>Go!</p></button></td></tr>"
-			dbinfo += markup;
+			let name = childData.marketInfo.marketName;
+			let address = childData.marketInfo.address;
+			let size = childData.marketInfo.storeType;
+			let zip = childData.marketInfo.zip;
+			let level = childData.marketInfo.marketLevel;
+
+			markets.push({name: name, address: address, size: size, zip: zip, level: level});
 		});
 
-		res.render('markets', {marketList: dbinfo});
+		res.render('markets', {markets: markets});
 	});
 });
 
