fe3a4dead7c8e3975f8325db44bd1dc9e1df3b11
Jason Cabrera <cabrerajason7@gmail.com>
2019-09-18 21:02:32 -0700

    moved auth to backend for markets and data page

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
 
-router.get('/', function(req, res, next) {
+if(!firebase.apps.length){
+	firebase.initializeApp(config.config);
+}
+
+const db = firebase.database();
+
+router.get('/', isAuthorized, function(req, res, next) {
 	res.render('data');
 });
+
+function isAuthorized(req, res, next){
+	firebase.auth().onAuthStateChanged(function(user) {
+		//user is signed in
+		if(user){
+			next();
+		}
+		
+		//not signed in
+		else{
+			res.render('admin-login');
+		}
+	});
+}
 
 module.exports = router;
--- routes/markets.js
+++ routes/markets.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+var config = require('./config.js');
 
-router.get('/', function(req, res, next) {
+if(!firebase.apps.length){
+	firebase.initializeApp(config.config);
+}
+
+const db = firebase.database();
+
+router.get('/', isAuthorized, function(req, res, next) {
 	res.render('markets');
 });
 
+function isAuthorized(req, res, next){
+	firebase.auth().onAuthStateChanged(function(user) {
+		//user is signed in 
+		if(user){
+			next();
+		}
+		
+		//not signed in 
+		else{
+			res.render('admin-login');
+		}
+	});
+}
 module.exports = router;
