38fb17d1423c204b583590cd771690f3e1daebd5
Jason Cabrera <cabrerajason7@gmail.com>
2019-09-18 21:04:17 -0700

    added config to routes

---
+++ routes/config.js
@@ -0,0 +1,10 @@
+config = {
+    apiKey: "AIzaSyB33kxrp6W3dihuRRFhgk2UFI9VSHCQvtI",
+    authDomain: "live-weller.firebaseapp.com",
+    databaseURL: "https://live-weller.firebaseio.com",
+    projectId: "live-weller",
+    storageBucket: "live-weller.appspot.com",
+    messagingSenderId: "147602049424"
+};
+
+exports.config = config;
