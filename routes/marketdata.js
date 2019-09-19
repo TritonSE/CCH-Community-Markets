111c8c59c7dc3cafba94614b78c5bf1a2027d27f
Declan Sullivan <declan@declansullivan.com>
2019-09-19 15:51:42 -0700

    moved Firebase reads for markets and marketdata pages to backend (#58)
    * Added dependencies for firebase
    
    * Removed firebase from public/js/markets
    
    * Displays markets in ejs from routes
    
    * Added config to routes
    
    * Removed clientside code for redirection
    
    * Added template tags to frontend
    
    * Moved marketdata code to backend
    
    * Removed extraneous lines
    
    * Removed unnecessary code
    
    * Removed redirectFunction
    
    * Altered config.js to export the object
    
    * var -> const and let, moved firebase info
    
    * Added partials
    
    * Added template structure to marketdata
    
    * Adjusted json declaration, less values declared
    
    * Removed onclick, added href

--- routes/marketdata.js
+++ routes/marketdata.js
@@ -1,8 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const firebase = require('firebase');
+const config = require('./config.js');
+const router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.render('marketdata');
+if (!firebase.apps.length) {
+    firebase.initializeApp(config.config);
+}
+// Creates connection to database.
+const db = firebase.database();
+// Links to head of database.
+const ref = db.ref("live_weller");
+// Links to markets list.
+const marketsRef = ref.child("markets");
+
+router.get('/:marketName', function(req, res, next) {
+    const marketName = req.params.marketName;
+
+    marketsRef.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            const childData = childSnapshot.val();
+            
+            let status = "";
+            let questions = []
+            let missed = [];
+
+            if (childData.marketInfo.marketName === marketName) {
+                status = "Questions to fix to get to level " + (parseInt(childData.marketInfo.marketLevel) + 1);
+                
+                if (childData.marketInfo.marketLevel === 3) {
+                    status = "Market is at Top Level!";
+                }
+
+                for (let key in childData.questions) {
+                    if (key !== "undefined") {
+                        questions.push({key: key, answer: childData.questions[key]});
+                    }
+                }
+
+                for (let key in childData.missedQuestions) {
+                    missed.push({key: childData.missedQuestions[key].replace('<span class=\"boldanswer\">', '').replace('</span>', '')});
+                }
+
+                res.render('marketdata', {
+                    name: childData.marketInfo.marketName,
+                    level: childData.marketInfo.marketLevel,
+                    address: childData.marketInfo.address,
+                    size: childData.marketInfo.storeType,
+                    first: childData.personalInfo.firstName,
+                    last: childData.personalInfo.lastName,
+                    email: childData.personalInfo.email,
+                    status,
+                    questions,
+                    missed
+                })
+            }
+        });		
+    });
 });
 
 module.exports = router;
