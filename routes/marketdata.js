ebebabdee65ca0671de129760d3c072d3fa760e3
Declan Sullivan <declan@declansullivan.com>
2019-09-23 13:11:55 -0700

    Replaced iteration with direct reference

--- routes/marketdata.js
+++ routes/marketdata.js
@@ -13,48 +13,42 @@ const ref = db.ref("live_weller");
 // Links to markets list.
 const marketsRef = ref.child("markets");
 
-router.get('/:marketName', function(req, res, next) {
-    const marketName = req.params.marketName;
+router.get('/:marketKey', function(req, res, next) {
+    const market = marketsRef.child(req.params.marketKey);
 
-    marketsRef.once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            const childData = childSnapshot.val();
-            
-            let status = "";
-            let questions = []
-            let missed = [];
+    market.once('value', function(snapshot) {
+        let questions = []
+        let missed = [];
+        const childData = snapshot.val();
 
-            if (childData.marketInfo.marketName === marketName) {
-                status = "Questions to fix to get to level " + (parseInt(childData.marketInfo.marketLevel) + 1);
+        let status = "Questions to fix to get to level " + (parseInt(childData.marketInfo.marketLevel) + 1);
                 
-                if (childData.marketInfo.marketLevel === 3) {
-                    status = "Market is at Top Level!";
-                }
+        if (childData.marketInfo.marketLevel === 3) {
+            status = "Market is at Top Level!";
+        }
 
-                for (let key in childData.questions) {
-                    if (key !== "undefined") {
-                        questions.push({key: key, answer: childData.questions[key]});
-                    }
-                }
-
-                for (let key in childData.missedQuestions) {
-                    missed.push({key: childData.missedQuestions[key].replace('<span class=\"boldanswer\">', '').replace('</span>', '')});
-                }
-
-                res.render('marketdata', {
-                    name: childData.marketInfo.marketName,
-                    level: childData.marketInfo.marketLevel,
-                    address: childData.marketInfo.address,
-                    size: childData.marketInfo.storeType,
-                    first: childData.personalInfo.firstName,
-                    last: childData.personalInfo.lastName,
-                    email: childData.personalInfo.email,
-                    status,
-                    questions,
-                    missed
-                })
+        for (const key in childData.questions) {
+            if (key !== "undefined") {
+                questions.push({key: key, answer: childData.questions[key]});
             }
-        });		
+        }
+
+        for (const key in childData.missedQuestions) {
+            missed.push({key: childData.missedQuestions[key].replace('<span class=\"boldanswer\">', '').replace('</span>', '')});
+        }
+
+        res.render('marketdata', {
+            name: childData.marketInfo.marketName,
+            level: childData.marketInfo.marketLevel,
+            address: childData.marketInfo.address,
+            size: childData.marketInfo.storeType,
+            first: childData.personalInfo.firstName,
+            last: childData.personalInfo.lastName,
+            email: childData.personalInfo.email,
+            status,
+            questions,
+            missed
+        })
     });
 });
 
