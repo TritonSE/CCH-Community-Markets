408b190a1651e0b88b81821edf7a1c42bb57dc32
Jason Cabrera <cabrerajason7@gmail.com>
2019-09-23 18:55:02 -0700

    added comments/updated variable types for assess.js

--- routes/assess.js
+++ routes/assess.js
@@ -4,26 +4,28 @@ var config = require('./config.js');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	
 	if(!firebase.apps.length) {
      firebase.initializeApp(config.info());
 	}
 
 	// Setup database communication.
-	let db = firebase.database();
-	let ref = db.ref("live_weller");
+	const db = firebase.database();
+	const ref = db.ref("live_weller");
 
 	// Move to sub-directory.
-	let marketsRef = ref.child("markets");
+	const marketsRef = ref.child("markets");
 	let marketOptions = [];
 
+	//add all market names to marketOptions
 	marketsRef.once('value', function(snapshot) {
    	snapshot.forEach(function(childSnapshot) {
-   		let childKey = childSnapshot.key;
-			marketOptions.push({marketName: childKey});
+   		let marketName = childSnapshot.key;
+			marketOptions.push({marketName: marketName});
    	});
 		
 		marketOptions.push({marketName: "NEW MARKET"});
+
+		//send marketOptions to assess.ejs
 		res.render('assess', {marketOptions});
 	});
 
