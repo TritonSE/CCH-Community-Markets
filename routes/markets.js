56063aed9e0486c8654439ca2619f856ec4bfac9
Jason Cabrera <cabrerajason7@gmail.com>
2019-09-27 00:42:52 -0700

    reverting changes

--- routes/markets.js
+++ routes/markets.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-if(!firebase.apps.length){
-	firebase.initializeApp(config.config);
-}
-const db = firebase.database();
-
-=======
->>>>>>> parent of c7779cb... moved auth check to backend for markets and data section
 router.get('/', function(req, res, next) {
 	res.render('markets');
 });
