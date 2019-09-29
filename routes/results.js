5c914160e01cf63d189901154a22826b8c14c84c
Declan Sullivan <declan@declansullivan.com>
2019-09-29 11:03:12 -0700

    Removed obselete results route

--- routes/results.js
+++ routes/results.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.render('results');
-});
-
 router.get('/:name/:level', function(req, res, next) {
 	res.render('results', {name: req.params.name, level: req.params.level});
 });
