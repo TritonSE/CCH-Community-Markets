4f5be8535d33532d06087280d7d02bf2030bbdf6
Declan Sullivan <declan@declansullivan.com>
2019-09-29 11:03:40 -0700

    Removed firebase from assess

--- routes/assess.js
+++ routes/assess.js
@@ -1,35 +1,19 @@
 const express = require('express');
-const firebase = require('firebase');
-const config = require('./config.js');
 const router = express.Router();
-
-//initializing firebase if its not already
-if(!firebase.apps.length) {
-   firebase.initializeApp(config.config);
-}
-
-// Setup database communication.
-const db = firebase.database();
-const ref = db.ref("live_weller");
-
-// Move to sub-directory.
-const marketsRef = ref.child("markets");
+const db = require('../db.js');
 
 router.get('/', function(req, res, next) {
-	let marketOptions = [];
+	db.exportAllMarkets().then(function(result) {
+		const markets = result.val();
+		let marketOptions = [];
 
-	//add all market names to marketOptions
-	marketsRef.once('value', function(snapshot) {
-   	snapshot.forEach(function(childSnapshot) {
-			marketOptions.push({marketName: childSnapshot.key});
-   	});
-		
+		for (const key in markets) {
+			marketOptions.push({marketName: key});
+		}
+
 		marketOptions.push({marketName: "NEW MARKET"});
-
-		//send marketOptions to assess.ejs
 		res.render('assess', {marketOptions});
 	});
-
 });
 
 module.exports = router;
