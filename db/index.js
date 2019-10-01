d529bf2f5c8c128b43078910bd6b4a5de052e0df
Declan Sullivan <declan@declansullivan.com>
2019-09-30 19:16:20 -0700

    Updated path to config file

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const firebase = require('firebase');
-const config = require('./config.js');
+const config = require('../config.js');
 
 if (!firebase.apps.length) {
 	firebase.initializeApp(config.config);
