20fdcbc8337e193632aebe171b9779dcb30b1bbf
Declan Sullivan <declan@declansullivan.com>
2019-10-08 21:07:30 -0700

    results -> assessment-results

--- app/routes/assessment-results.js
+++ app/routes/assessment-results.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:name/:level', (req, res, next) => {
-  res.render('results', { name: req.params.name, level: req.params.level });
+  res.render('assessment-results', { name: req.params.name, level: req.params.level });
 });
 
 module.exports = router;
