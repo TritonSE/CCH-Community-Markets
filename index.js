62104e2e427182187ce1afade110eccdc3bf25b7
Declan Sullivan <declan@declansullivan.com>
2019-10-08 21:07:58 -0700

    Updated server uses

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ server.use(express.static(path.join(__dirname, 'app/public')));
 
 // app routes
 server.use('/', require('./app/routes/index'));
-server.use('/assess', require('./app/routes/assess'));
-server.use('/markets', require('./app/routes/markets'));
-server.use('/results', require('./app/routes/results'));
-server.use('/data', require('./app/routes/data'));
-server.use('/marketdata', require('./app/routes/marketdata'));
-server.use('/admin-login', require('./app/routes/admin-login'));
-server.use('/submit-assess', require('./app/routes/submit-assess'));
+server.use('/assess', require('./app/routes/begin-assessment'));
+server.use('/markets', require('./app/routes/market-table'));
+server.use('/results', require('./app/routes/assessment-results'));
+server.use('/data', require('./app/routes/visualize-markets'));
+server.use('/market', require('./app/routes/individual-market-info'));
+server.use('/login', require('./app/routes/login'));
+server.use('/submit-assessment', require('./app/routes/submit-assessment'));
 
 server.listen(app.config.port, () => {
   log.info(`Started server on port ${app.config.port}`);
