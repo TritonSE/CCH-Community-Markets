8c3496c659136c96fd518ad7c3581c064132e295
Declan Sullivan <declan@declansullivan.com>
2019-10-10 23:49:54 -0700

    added new get paths to assessment

---
+++ app/routes/assessment.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const db = require('../db');
+const log = require('../logger');
+
+const router = express.Router();
+
+// Load form page for assessment.
+router.get('/', (req, res, next) => {
+  db.getAllMarkets().then((markets) => {
+    const marketOptions = [];
+
+    for (const key in markets) {
+      marketOptions.push({ marketName: key });
+    }
+
+    marketOptions.push({ marketName: 'NEW MARKET' });
+    res.render('assess-market', { marketOptions });
+  }).catch((error) => {
+    log.error(error);
+  });
+});
+
+// Load results page for assessment.
+router.get('/results/:name/:level', (req, res, next) => {
+  res.render('assessment-results', { name: req.params.name, level: req.params.level });
+});
+
+// Post request to update database.
+router.post('/submit', (req, res) => {
+  const info = req.body.data;
+  if (info.new === true) db.addNewMarket(info);
+  else db.updateExistingMarket(info);
+  res.json({ error: null });
+});
+
+module.exports = router;
