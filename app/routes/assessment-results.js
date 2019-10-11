1527b18bcdad9d5e50ebec54b17801d0f5500296
Declan Sullivan <declan@declansullivan.com>
2019-10-10 23:48:45 -0700

    deleted obselete routes

--- app/routes/assessment-results.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/:name/:level', (req, res, next) => {
-  res.render('assessment-results', { name: req.params.name, level: req.params.level });
-});
-
-module.exports = router;
